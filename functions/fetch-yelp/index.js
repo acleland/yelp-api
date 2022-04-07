bd6a18071b1f476e1313504b6821598160dd23f0
Andrea Cleland <andreaclelan@gmail.com>
2022-04-07 12:35:06 -0700

    got it working, reports an error if no zip is provided

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -3,6 +3,28 @@ require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
   // add code here to fetch data from yelp API
+  const zip = event.queryStringParameters.zip;
+  const search = event.queryStringParameters.search;
+  try {
+    const resp = await fetch(
+      `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${search}`,
+      {
+        headers: { Authorization: `Bearer ${process.env.YELP_API_KEY}` },
+      }
+    );
+    const data = await resp.json();
+    const json = JSON.stringify({ data });
+
+    return {
+      statusCode: 200,
+      body: json,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
   // be sure to include the parameters from event.queryStringParameters
 };
 
--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import { fetchBusinesses } from './services/yelp';
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   // TODO -- add state for zip / search and add event listeners to the inputs
+  const [zipCode, setZipCode] = useState('97202');
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchBusinesses();
-      setBusinesses(data);
+      setBusinesses(data.data.businesses);
       setLoading(false);
     };
     fetchData();
@@ -20,19 +23,45 @@ function App() {
 
   // TODO -- add event for button click to handle calling fetchBusinesses with zip / search
 
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      if (zipCode) {
+        const data = await fetchBusinesses(zipCode, searchQuery);
+        setBusinesses(data.data.businesses);
+        setLoading(false);
+        setError('');
+      } else {
+        setError('You must enter a zip code');
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
   return (
     <div className="App">
       <h1>Alchemy Restaurant Finder</h1>
+      {error && <p>{error}</p>}
       <div className="query-form">
         <div className="form-control">
           <label>Zip:</label>
-          <input type="text" placeholder="zip" />
+          <input
+            type="text"
+            placeholder="zip"
+            value={zipCode}
+            onChange={(e) => setZipCode(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label>Query:</label>
-          <input type="text" placeholder="Search..." />
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
         </div>
-        <button>Search</button>
+        <button onClick={handleClick}>Search</button>
       </div>
       {loading && <div className="loader"></div>}
       {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
